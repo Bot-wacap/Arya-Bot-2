2497fd233803058aa30c89ce759e11c2ad87650c
Bot-wacap <75020738+Bot-wacap@users.noreply.github.com>
2021-01-11 17:14:35 +0700

    Update nulis.js

--- plugins/nulis.js
+++ plugins/nulis.js
@@ -50,7 +50,7 @@ let handler  = async (m, { conn, args }) => {
   ])
   .on('error', e => conn.reply(m.chat, util.format(e), m))
   .on('exit', () => {
-    conn.sendFile(m.chat, outputPath, 'nulis.jpg', 'Nih cukk tulisannya.. Mager Lu? \n*Instagram:* @Uwiw_Bocah')
+    conn.sendFile(m.chat, outputPath, 'nulis.jpg', 'Nih cukk tulisannya.. Mager Lu? \n*Instagram:* @cuppu_gamingch')
   })
 }
 handler.command = /^nulis$/i
